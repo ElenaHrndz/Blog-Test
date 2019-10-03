97985a7c2cc5cdeca1b0f05e351da4abdf28e7df
Elena Hernandez <hrndzelena@gmail.com>
2019-10-03 14:44:40 -0500

    error and success post test
    check if uses exist, if exist create a post if not return error

--- src/endpoints/posts/index.spec.js
+++ src/endpoints/posts/index.spec.js
@@ -38,5 +38,33 @@ describe('Endpoints', () =>{
                 ['https://jsonplaceholder.typicode.com/posts', post]
             ])
         })
+        it('Shoudl not create if user id dose not exist', async () => {
+            const mockUsers = [
+                { id: 1 },
+                { id: 2 }
+            ]
+            const post = {
+                userId: 3,
+                title: "Title",
+                body: "Body post"
+            }
+            const req = {
+                body: post,
+            }
+            const res ={
+                status: jest.fn().mockReturnThis(),
+                send: jest.fn(),
+                sendStatus: jest.fn()
+            }
+            const axios = {
+                get: jest.fn().mockResolvedValue({ data: mockUsers }),
+                post: jest.fn().mockResolvedValue({ data: { id: 1000 }}),
+            }
+            await postHandlers({ axios }).post(req, res)
+            expect(axios.post.mock.calls).toEqual([])
+            expect(res.sendStatus.mock.calls).toEqual([
+                [500]
+            ])
+        })
     })
 })
